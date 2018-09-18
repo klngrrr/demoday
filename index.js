fcfef9de20af1b71d86df2d422ba577747ed2b21
fabianosilva24 <fabiano.jaragua24@hotmail.com>
2018-09-18 10:25:18 -0300

    demoday com express

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+app.use(bodyParser.urlencoded());
+
+app.get('', (req, res) => {
+    res.render('index');
+});
+
+app.get('/views/Nossotrabalho.ejs', (req, res) => {
+    res.render('NossoTrabalho');
+});
+app.get('/views/Parceiros.ejs', (req, res) => {
+    res.render('Parceiros');
+});
+
+
+app.listen(4000, () => {
+    console.log('Servidor inicializado')
+});
